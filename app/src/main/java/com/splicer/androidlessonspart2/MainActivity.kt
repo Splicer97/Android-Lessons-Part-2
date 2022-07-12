d7654694e160cdc723ec24cabf7b1aeff8a74c51
Nikita_Kovantsov <kovancov@gmail.com>
2022-07-12 21:46:01 +0300

    lesson 3 tab layout

--- app/src/main/java/com/splicer/androidlessonspart2/MainActivity.kt
+++ app/src/main/java/com/splicer/androidlessonspart2/MainActivity.kt
@@ -2,10 +2,33 @@ package com.splicer.androidlessonspart2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.google.android.material.tabs.TabLayout
+import com.splicer.androidlessonspart2.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private var fragList = listOf(
+        Fragment1.newInstance(),
+        Fragment2.newInstance(),
+        Fragment3.newInstance()
+    )
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.tb.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener {
+            override fun onTabSelected(tab: TabLayout.Tab?) {
+                supportFragmentManager.beginTransaction()
+                    .replace(R.id.placeHolder, fragList[tab?.position!!]).commit()
+            }
+
+            override fun onTabUnselected(tab: TabLayout.Tab?) {
+            }
+
+            override fun onTabReselected(tab: TabLayout.Tab?) {
+            }
+
+        })
     }
 }
