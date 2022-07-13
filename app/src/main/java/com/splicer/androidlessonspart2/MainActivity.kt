79efa6173b54d9e55fefa27309390ea58f05aee5
Nikita_Kovantsov <kovancov@gmail.com>
2022-07-13 19:19:44 +0300

    lesson_2_view_pager_2

--- app/src/main/java/com/splicer/androidlessonspart2/MainActivity.kt
+++ app/src/main/java/com/splicer/androidlessonspart2/MainActivity.kt
@@ -2,8 +2,8 @@ package com.splicer.androidlessonspart2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Toast
 import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayoutMediator
 import com.splicer.androidlessonspart2.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -12,24 +12,34 @@ class MainActivity : AppCompatActivity() {
         Fragment2.newInstance(),
         Fragment3.newInstance()
     )
+    private var fragTitleList = listOf(
+        "Item 1",
+        "Item 2",
+        "Item 3",
+    )
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        binding.tb.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener {
-            override fun onTabSelected(tab: TabLayout.Tab?) {
-                supportFragmentManager.beginTransaction()
-                    .replace(R.id.placeHolder, fragList[tab?.position!!]).commit()
-            }
-
-            override fun onTabUnselected(tab: TabLayout.Tab?) {
-            }
-
-            override fun onTabReselected(tab: TabLayout.Tab?) {
-            }
-
-        })
+        val adapter = VpAdapter(this, fragList)
+        binding.vp2.adapter = adapter
+        TabLayoutMediator(binding.tb, binding.vp2) {
+                tab, pos -> tab.text = fragTitleList[pos]
+        }.attach()
+//        binding.tb.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener {
+//            override fun onTabSelected(tab: TabLayout.Tab?) {
+//                supportFragmentManager.beginTransaction()
+//                    .replace(R.id.vp2, fragList[tab?.position!!]).commit()
+//            }
+//
+//            override fun onTabUnselected(tab: TabLayout.Tab?) {
+//            }
+//
+//            override fun onTabReselected(tab: TabLayout.Tab?) {
+//            }
+//
+//        })
     }
 }
